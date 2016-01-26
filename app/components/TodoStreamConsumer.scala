106af57e2c0c0d37ecc6b5edcbb46607d69f2093
mwielocha <mikolaj.wielocha@yougov.com>
2016-01-26 14:08:57 +0100

    add exclusive queues

--- app/components/TodoStreamConsumer.scala
+++ app/components/TodoStreamConsumer.scala
@@ -6,9 +6,10 @@ import javax.inject.{Inject, Singleton}
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Sink}
+import com.spingo.op_rabbit.Binding._
 import com.spingo.op_rabbit.Directives._
 import com.spingo.op_rabbit.stream.RabbitSource
-import com.spingo.op_rabbit.{ConnectionParams, Delivery, RabbitControl}
+import com.spingo.op_rabbit.{Exchange, ConnectionParams, Delivery, RabbitControl}
 import com.timcharper.acked.AckedFlow
 import logging.LoggingComponent
 import model.TodoEvent
@@ -26,12 +27,6 @@ class TodoStreamConsumer @Inject()(val configuration: Configuration,
                                    val todoRepository: TodoRepository)
                                   (implicit val actorSystem: ActorSystem, val ec: ExecutionContext) extends LoggingComponent {
 
-  private final val queueName = "todo-notification-queue"
-
-  private val utf8 = Charset.forName("UTF-8")
-
-  logger.info(s"Declaring queue $queueName")
-
   val rabbitControl = actorSystem.actorOf(Props(
     classOf[RabbitControl],
     ConnectionParams.fromConfig(
@@ -40,12 +35,9 @@ class TodoStreamConsumer @Inject()(val configuration: Configuration,
 
   val source = RabbitSource(
     rabbitControl,
-    channel(qos = 3),
-    consume(queue(
-      queue = queueName,
-      durable = true,
-      exclusive = false,
-      autoDelete = false)),
+    channel(),
+    consume(fanout(queue("", exclusive = true),
+      Exchange.fanout("todo-notification-exchange"))),
     body(as[String])
   )
 
